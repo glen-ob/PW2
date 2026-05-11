8eb06043bfd311138704930c9fd06db2223c1f95
glen ordóñez <149749777+glen-ob@users.noreply.github.com>
2026-05-10 23:45:41 -0600

    AAAGUGH

--- backend/services/server.js
+++ backend/services/server.js
@@ -25,12 +25,12 @@ const app = express();
 connectDB();
 
 // Middlewares
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     credentials: true
+// }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 
 
